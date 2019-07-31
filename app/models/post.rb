6320417bfbd05f68e5a4acdce9742837d65fa7a6
Savannah C <sgcorrell@gmail.com>
2019-07-31 19:24:34 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,6 @@ class Post < ActiveRecord::Base
   validate :is_title_case
   
   before_validation :make_title_case
-  
-  before_save :email_author_about_post
 
   private
 
@@ -19,6 +17,4 @@ class Post < ActiveRecord::Base
     self.title = self.title.titlecase
   end
   
-  def email_author_about_post
-  end
 end
