568faa1bb666f4beab091f2211717c0bf79e0a8a
Savannah C <sgcorrell@gmail.com>
2019-07-31 19:22:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case
   
-  before_validation :make_title_case 
+  before_validation :make_title_case
+  
+  before_save :email_author_about_post
 
   private
 
